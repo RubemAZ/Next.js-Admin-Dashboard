59ccdbc47f8a36f6ed06b91a2266612afaf8c8be
Rubem Mazetto <Raz.rubem@gmail.com>
2024-11-05 09:03:41 -0300

    initial setup

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,5 @@
 import "./styles/globals.css";
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+
 
 export default function RootLayout({
   children,
@@ -9,13 +8,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body >
-          <Header />
-
-          <main>{children}</main>
-
-          <Footer />
-      </body>
+      <body >{children}</body>
     </html>
   );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,14 @@
 import Layout from './layout';
-import { Button } from 'antd';
-
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 export default function Home() {
     return (
         <Layout>
-            <div>
-                <h1>Bem-vindo ao nosso Hotsite!</h1>
-                <p>Este é um template inicial utilizando Next.js e Ant Design.</p>
-                <Button type="primary" >Saiba Mais</Button>
-            </div>
+            <Header />
+            <main>
+                <p>Hello there.</p>
+            </main>
+            <Footer />
         </Layout>
     );
 }
